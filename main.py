240218e9f44e33658c03bea60bd8bb3c55cf41f8
Artem <artemkush1@gmail.com>
2020-01-28 16:59:28 +0300

    fix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import fitz
 import os
-from tqdm import tqdm
+# from tqdm import tqdm
+import progressbar
 
 
 d = str(input('Перетащите файл в окно: '))
 pdf_document = fitz.open(d)
-for current_page in tqdm(range(len(pdf_document))):
+
+bar = progressbar.ProgressBar(len(pdf_document)).start()
+
+for current_page in range(len(pdf_document)):
     for image in pdf_document.getPageImageList(current_page):
         xref = image[0]
         pix = fitz.Pixmap(pdf_document, xref)
@@ -16,5 +20,6 @@ for current_page in tqdm(range(len(pdf_document))):
         pix1 = fitz.Pixmap(fitz.csRGB, pix)
         # pix1.writeImage(f"img/page%s-%s.jpg" % (current_page, xref))
         pix1.writePNG(f"img/page%s-%s.png" % (current_page, xref))
+        bar.update(current_page)
 
 
