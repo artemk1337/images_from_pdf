7e16322e1aa92d967a615f21b21d9065709898c1
Artem <artemkush1@gmail.com>
2020-01-28 16:48:29 +0300

    fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import fitz
 import os
+from tqdm import tqdm
 
 
 d = str(input('Перетащите файл в окно: '))
 pdf_document = fitz.open(d)
-for current_page in range(len(pdf_document)):
+for current_page in tqdm(range(len(pdf_document))):
     for image in pdf_document.getPageImageList(current_page):
         xref = image[0]
         pix = fitz.Pixmap(pdf_document, xref)
@@ -12,10 +13,8 @@ for current_page in range(len(pdf_document)):
         if not os.path.isdir(f'img'):
             os.mkdir(f'img')
 
-        if pix.n < 5:        # this is GRAY or RGB
-            pix.writePNG(f"img/page%s-%s.jpg" % (current_page, xref))
-        else:                # CMYK: convert to RGB first
-            pix1 = fitz.Pixmap(fitz.csRGB, pix)
-            pix1.writePNG(f"img/page%s-%s.jpg" % (current_page, xref))
+        pix1 = fitz.Pixmap(fitz.csRGB, pix)
+        # pix1.writeImage(f"img/page%s-%s.jpg" % (current_page, xref))
+        pix1.writePNG(f"img/page%s-%s.png" % (current_page, xref))
 
 
